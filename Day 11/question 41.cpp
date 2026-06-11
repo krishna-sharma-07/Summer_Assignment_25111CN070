d2faa477ff5c43acb32b29b15ae6779cba684705
Krishna Sharma <sharmakrishna5181@gmail.com>
2026-06-11 08:38:02 +0530

    Day 11 cpp only

---
+++ Day 11/question 41.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int sum(int a , int b){
+    return a + b;
+}
+int main(){
+    int x , y;
+    cin >> x >> y;
+    cout << "sum = " << sum(x , y);
+    return 0;
+
+}
---
+++ Day 11/question 42.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int maximum(int a , int b){
+    if(a > b)
+    return a;
+    else
+    return b;
+}
+int main(){
+    int x , y;
+    cin >> x >> y;
+    cout << "maximum =" << maximum(x , y);
+    return 0;
+}
